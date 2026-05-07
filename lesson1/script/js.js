90b1af559b3647544ba5bd68a30c33d31cac3ec0
Pugach Vsevolod <l901752@gmail.com>
2026-05-07 10:17:29 +0300

    Add files via upload

---
+++ lesson1/script/js.js
@@ -0,0 +1,66 @@
+// console.log("Hello, World!");
+// console.info("Hello JS #2");
+// console.error("Hello JS #3");
+// console.warn("Hello JS #4");
+//
+// var myFirstVar;
+// myFirstVar = 10;
+// console.log(myFirstVar);
+//
+// var mySecondVar = '2';
+// console.log("Змінна: " + mySecondVar);
+// console.log(mySecondVar + myFirstVar);
+// console.log(myFirstVar - mySecondVar);
+//
+// // var first, second = 2, "4";
+// var int = 10;
+// var float = 10.2;
+// var BOOL = true; // false
+// var str = "bagel"
+// var nothing;
+// nothing = null;
+//
+// num = 2;
+// var num;
+// console.log(num);
+//
+// let num = 13;
+// num = '14';
+// console.log(num);
+//
+// const Pi = 3.14;
+
+//
+// let a = 21, b = 4;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+// let a, b;
+// a = 2 + 4;
+// b = 3 * (a = 10 + 3);
+// a = a + 1;
+// a += 1;
+//
+// a++; //інкремент
+// a--; //декремент
+//
+// ++a;
+// --a;
+//
+// let a = 2, b;
+//
+// b = ++a;
+// console.log(a);
+let a = '2', b = '3';
+console.log((a-0)+(b-0));
+console.log(Number(a)+Number(b));
+//щеплення, інтерполяція
+
+let name = 'Ivan';
+console.log("Привіт " + name + "!");
+let skill = "WEB";
+
+console.log(`Привіт ${name}! Твій найкращий скіл це - ${skill} ${2*4}`);
